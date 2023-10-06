04a488764f42e5e72ad1178b8eb53af07e3092d6
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-10-05 21:01:06 -0300

    Create UserInfo Module

---
+++ Bot.App/Modules/UserInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace Bot.App.Modules;
+
+using Simple.BotUtils.Controllers;
+
+/// <summary>
+/// Module to handle user info commands
+/// </summary>
+public class UserInfo : IController
+{
+    public void Me(CommandArguments args)
+    {
+        var text = $@"User Info:
+> ChatId: {args.ChatID}
+> FromId: {args.FromID}
+
+MessageInfo:
+> MessageDate: {args.Message.Date}
+> FirstName: {args.Message.From?.FirstName}
+> LastName: {args.Message.From?.LastName}
+> Username: {args.Message.From?.Username}
+> Id: {args.Message.From?.Id}
+";
+
+        args.Message.Reply(text);
+    }
+}
