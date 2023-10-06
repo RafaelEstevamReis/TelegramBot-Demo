52ad71b3a40f48579188d2c61a8f37598277280a
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-10-05 21:00:24 -0300

    Update Config.cs

--- Bot.App/Core/Config.cs
+++ Bot.App/Core/Config.cs
@@ -4,6 +4,8 @@ using Simple.BotUtils.Data;
 
 public class Config : ConfigBase<Config>
 {
+    public const long ADMIN_TELEGRAM_ID = 1234;
+
     public string TelegramToken { get; set; }
     public string DatabasePath { get; set; }
     public string LogsPath { get; set; }
